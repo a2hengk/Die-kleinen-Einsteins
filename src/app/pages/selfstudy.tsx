6d71d484e6814eb93c7d1e3cc90bfb2893b94bb4
Kevin <heng.kevin05@gmail.com>
2026-03-12 13:05:41 +0000

    looks like the konzept now

--- src/app/pages/selfstudy.tsx
+++ src/app/pages/selfstudy.tsx
@@ -5,10 +5,17 @@ import stylesBody from '../styles/overview-styles/body.module.css';
 export default function SelfStudy() {
   return (
     <div className={stylesContainer.container}>
-      <h1>Self Study</h1>
+      <h1 className={stylesContainer.title}>Self Study</h1>
       <main className={stylesBody.body}>
         <div className={stylesStatusbar.statusbar}>
-          <span className={stylesStatusbar.correctCount}>Correct: 0</span>
+          <span>Correct: <span className={stylesStatusbar.correctCount}>0</span></span>
+          <span>Wrong: <span className={stylesStatusbar.wrongCount}>0</span></span>
+        </div>
+        <div className={stylesContainer.card}></div>
+        <div className={stylesContainer.buttons}>
+          <button className={stylesContainer.right}>Correct</button>
+          <button className={stylesContainer.wrong}>Wrong</button>
+          <button className={stylesContainer.showAnswer}>Show Answer</button>
         </div>
       </main>
     </div>
