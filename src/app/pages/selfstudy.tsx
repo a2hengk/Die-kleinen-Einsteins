bcbe9a43714fa46c95b7c2084c007ebd79e84e3c
Kevin <heng.kevin05@gmail.com>
2026-03-12 12:49:32 +0000

    change file names & add elements in selfstudy page

--- src/app/pages/selfstudy.tsx
+++ src/app/pages/selfstudy.tsx
@@ -1,8 +1,16 @@
+import stylesContainer from '../styles/selfstudy-styles/container.module.css';
+import stylesStatusbar from '../styles/selfstudy-styles/statusbar.module.css';
+import stylesBody from '../styles/overview-styles/body.module.css';
+
 export default function SelfStudy() {
   return (
-    <div className="self-study">
+    <div className={stylesContainer.container}>
       <h1>Self Study</h1>
-      <p>This is the self study page.</p>
+      <main className={stylesBody.body}>
+        <div className={stylesStatusbar.statusbar}>
+          <span className={stylesStatusbar.correctCount}>Correct: 0</span>
+        </div>
+      </main>
     </div>
   );
 }
